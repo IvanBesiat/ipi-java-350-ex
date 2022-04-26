b97fd1996e4736e37dfddab4b250a1120be53d0e
IvanBesiat <ivan.besiat@gmail.com>
2022-04-26 14:35:17 +0200

    ajout de test pour la methode getNbrAnneeAnciennetee

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,16 @@
+import com.ipiecoles.java.java350.model.Employe;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class EmployeTest {
+
+    @Test
+    public void testGetNombreAnneeEncienneteBadDateEmbauche(){
+        //Given
+        Employe employeTest = new Employe();
+
+        //When
+        Boolean ancienneteOk = Employe.
+    }
+}
