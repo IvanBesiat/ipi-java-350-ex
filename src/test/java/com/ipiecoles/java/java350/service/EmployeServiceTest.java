b9365d82669b8a8dcbb63671520dbb17f6a83ecf
IvanBesiat <ivan.besiat@gmail.com>
2022-06-20 12:46:02 +0200

    ajout de test sur le service

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -2,6 +2,7 @@ package com.ipiecoles.java.java350.service;
 
 import com.ipiecoles.java.java350.exception.EmployeException;
 import com.ipiecoles.java.java350.model.Employe;
+import com.ipiecoles.java.java350.model.Entreprise;
 import com.ipiecoles.java.java350.model.NiveauEtude;
 import com.ipiecoles.java.java350.model.Poste;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
@@ -41,10 +42,48 @@ public class EmployeServiceTest {
         Assertions.assertThat(employeTest.getMatricule()).isEqualTo("M00001");
         Assertions.assertThat(employeTest.getNom()).isEqualTo("Besiat");
         Assertions.assertThat(employeTest.getPrenom()).isEqualTo("Ivan");
-        Assertions.assertThat(employeTest.getSalaire()).isEqualTo(2129.708);
-        Assertions.assertThat(employeTest.getPerformance()).isEqualTo(1);
-        Assertions.assertThat(employeTest.getNombreAnneeAnciennete()).isEqualTo(0);
+        Assertions.assertThat(employeTest.getSalaire()).isEqualTo(2129.71);
+        Assertions.assertThat(employeTest.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
         Assertions.assertThat(employeTest.getDateEmbauche()).isEqualTo(LocalDate.now());
         Assertions.assertThat(employeTest.getTempsPartiel()).isEqualTo((1.0));
     }
+
+    @Test
+    public void testEmbaucheEmployeWith1Employe() throws EmployeException {
+        //given
+        Employe manager = new Employe("BESIAT","Ivan","M00001", LocalDate.now(),2500d,2,1.0);
+        employeRepository.save(manager);
+        //when
+        employeService.embaucheEmploye("Besiat", "Ivan", Poste.COMMERCIAL, NiveauEtude.DOCTORAT, 1.0);
+        //then
+        Employe employeTest = employeRepository.findByMatricule("C00002");
+        Assertions.assertThat(employeRepository.findLastMatricule()).isEqualTo("00002");
+        Assertions.assertThat(employeTest.getMatricule()).isEqualTo("C00002");
+        Assertions.assertThat(employeTest.getNom()).isEqualTo("Besiat");
+        Assertions.assertThat(employeTest.getPrenom()).isEqualTo("Ivan");
+        Assertions.assertThat(employeTest.getSalaire()).isEqualTo(2586.07);
+        Assertions.assertThat(employeTest.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
+        Assertions.assertThat(employeTest.getDateEmbauche()).isEqualTo(LocalDate.now());
+        Assertions.assertThat(employeTest.getTempsPartiel()).isEqualTo((1.0));
+    }
+
+//    @Test
+//    public void testEmbaucheEmployeWithMaxMatricule() throws EmployeException {
+//        //given
+//        Employe manager = new Employe("BESIAT","Ivan","M99999", LocalDate.now(),2500d,2,1.0);
+//        employeRepository.save(manager);
+//        //when
+//        employeService.embaucheEmploye("Besiat", "Ivan", Poste.COMMERCIAL, NiveauEtude.DOCTORAT, 1.0);
+//        //then
+//        Employe employeTest = employeRepository.findByMatricule("C00002");
+//        Assertions.assertThat(employeRepository.findLastMatricule()).isEqualTo("00002");
+//        Assertions.assertThat(employeTest.getMatricule()).isEqualTo("C00002");
+//        Assertions.assertThat(employeTest.getNom()).isEqualTo("Besiat");
+//        Assertions.assertThat(employeTest.getPrenom()).isEqualTo("Ivan");
+//        Assertions.assertThat(employeTest.getSalaire()).isEqualTo(2586.07);
+//        Assertions.assertThat(employeTest.getPerformance()).isEqualTo(Entreprise.PERFORMANCE_BASE);
+//        Assertions.assertThat(employeTest.getDateEmbauche()).isEqualTo(LocalDate.now());
+//        Assertions.assertThat(employeTest.getTempsPartiel()).isEqualTo((1.0));
+//
+//    }
 }
